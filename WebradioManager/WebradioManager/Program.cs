42933b2c41b2a1a1b52d1490a0ff1f0a1ad01489
Simon Menetrey <simon.menetrey@gmail.com>
2014-04-03 11:59:31 +0200

    03.04.2014 noon
    - Change playlist mokup
    - Create server mokup
    - Edit DB

--- WebradioManager/WebradioManager/Program.cs
+++ WebradioManager/WebradioManager/Program.cs
@@ -16,7 +16,7 @@ namespace WebradioManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SelectionView());
+            Application.Run(new AdminView());
         }
     }
 }
